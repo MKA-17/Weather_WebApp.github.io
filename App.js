57bf731ba1fd833d3562ccad0586e2d962317d24
Muhammad Khizar Aslam <2020ee30@student.uet.edu.pk>
2022-08-13 23:55:20 +0500

    .

--- App.js
+++ App.js
@@ -11,21 +11,31 @@ class Weather {
             .then(resp => resp.json())
             .then(data => {
 
+                (document.querySelector("#infocard").style.display) = 'block';
+                document.querySelector("#errordiv").style.display = "none";
+                console.log("inside then 2")
                 this.Display_Weather(data)
-            }).catch(() => { alert("Something going wrong.") })
+
+            }).catch((x) => {
+                console.log("Something going wrong.", x);
+                (document.querySelector("#infocard").style.display) = 'none';
+                document.querySelector("#errordiv").style.display = "block";
+
+            })
 
     }
 
     Display_Weather(data) {
 
-        console.log("displaying data ", data)
+        console.log("INside Display weather method ", data)
+        document.querySelector("#errordiv").innerHTML = data.message
         document.querySelector(".cityname").innerHTML = "Weather in " + data.name;
         document.querySelector(".temperature").innerHTML = (data.main.temp - 273.16).toFixed(2) + "°C";
         document.querySelector(".humidity").innerHTML = "Humidity: " + data.main.humidity + "%";
         document.querySelector(".clouddiscrip").innerHTML = data.weather[0].description;
         document.querySelector(".windspeed").innerHTML = "Wind: " + data.wind.speed;
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + data.name + "' )"
-    
+
     }
 
 
@@ -33,10 +43,11 @@ class Weather {
 
 
 
-
+document.querySelector("#errordiv").style.display = "none";
 let weather = new Weather();
 let current_loc_btn = document.querySelector(".currentlocbtn");
 current_loc_btn.style.display = "none";
+
 
 //For default weather at Current Location
 
@@ -46,7 +57,21 @@ function locating(x) {
 
     fetch("https://api.opencagedata.com/geocode/v1/json?q=" + x.coords.latitude + "+" + x.coords.longitude + "&key=b2d663fa361d47cb824715cd6997b142")
         .then(resp => resp.json())
-        .then(data => (weather.fetch_api(data.results[0].components.city)))
+        .then(data => {
+            console.log(data);
+            (document.querySelector("#infocard").style.display) = 'block';
+            document.querySelector("#errordiv").style.display = "none";
+
+            (weather.fetch_api(data.results[0].components.city));
+        }
+        )
+        .catch(x => {
+            document.querySelector("#errordiv").innerHTML = "Something went wrong."
+                (document.querySelector("#infocard").style.display) = 'none';
+            document.querySelector("#errordiv").style.display = "block";
+
+
+        })
 }
 
 
